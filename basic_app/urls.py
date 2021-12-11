6b47f2e74b482875d778fe10037b713466674c6c
J-Appleg8 <84997748+J-Appleg8@users.noreply.github.com>
2021-12-11 15:25:42 -0800

    fixed urls

--- basic_app/urls.py
+++ basic_app/urls.py
@@ -5,5 +5,5 @@ app_name = "basic_app"
 
 urlpatterns = [
     path("register/", views.register, name="register"),
-    path("login/", views.login, name="login"),
+    # path("login/", views.login, name="login"),
 ]
